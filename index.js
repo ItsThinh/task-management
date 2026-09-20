fccfa7b162366ae41c8fb997aa2e4e89f5a347eb
Thinh <102139683+ItsThinh@users.noreply.github.com>
2026-09-20 22:43:58 +0700

    feat: install CORS

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const database = require("./config/database");
 const dns = require('node:dns/promises');
 require("dotenv").config();
 const route = require("./api/v1/routes/index.route");
+const cors = require("cors");
 
 dns.setServers(['1.1.1.1', '8.8.8.8']);
 database.connect();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 route(app);
 
